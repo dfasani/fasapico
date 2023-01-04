9137acaa52e5b6cc43a0ab1af0dce59364a04a37
dfasani <david.fasani@gmail.com>
2023-01-04 10:59:13 +0100

    Add files via upload
    Simple function to read distance from the grove ultrasonic distance ranger. Due to the fact that a single wire is used for T and R.

---
+++ GroveUltrasonicRanger.py
@@ -0,0 +1,29 @@
+from machine import Pin
+import utime
+
+def get_distance(pin_number):
+    
+    Pin(pin_number, Pin.OUT).low()
+    utime.sleep_us(2)
+    Pin(pin_number, Pin.OUT).high()
+    utime.sleep_us(5)
+    Pin(pin_number, Pin.OUT).low()
+    
+    while Pin(pin_number, Pin.IN).value() == 0:
+        signaloff = utime.ticks_us()
+    while Pin(pin_number, Pin.IN).value() == 1:
+        signalon = utime.ticks_us()
+    
+    timepassed = signalon - signaloff
+    distance = (timepassed * 0.0343) / 2
+    
+    return distance
+
+while True:
+    print("The distance from object is", get_distance(pin_number=7), "cm")
+    utime.sleep(0.2)
+
+
+
+
+

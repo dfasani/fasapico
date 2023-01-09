9b8adc0d6eb25936e017266f6e197dcaae70c62a
dfasani <david.fasani@gmail.com>
2023-01-09 12:08:39 +0100

    1ere version

---
+++ examples/GPS.py
@@ -0,0 +1,18 @@
+from machine import Pin, UART
+from time import sleep
+from micropyGPS import MicropyGPS
+
+gpsModule = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
+my_gps = MicropyGPS()
+    
+while True:
+    
+    #maj donnees GPS
+    for x in str(gpsModule.readline()):
+        my_gps.update(x)
+    
+    print( "latitude", my_gps.longitude)
+    print( "longitude", my_gps.longitude)
+    print()
+
+    sleep(1)

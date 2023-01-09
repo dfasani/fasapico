c4e974d008c05ebc1abaeb450ae791c4bc5d5231
dfasani <david.fasani@gmail.com>
2023-01-09 09:39:44 +0100

    Create read_potentiometer.py

---
+++ examples/read_potentiometer.py
@@ -0,0 +1,8 @@
+import machine, utime
+ 
+analog_value = machine.ADC(27)
+ 
+while True:
+    reading = analog_value.read_u16()   #value between [0-65535]  
+    print("ADC: ",reading)
+    utime.sleep(0.2)                    #do not read to frequentrly

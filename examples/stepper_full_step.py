846b00877ad80593afa04e2ef684c63d2f04599c
dfasani <david.fasani@gmail.com>
2023-01-09 15:56:17 +0100

    sleep 2 ms

--- examples/stepper_full_step.py
+++ examples/stepper_full_step.py
@@ -19,4 +19,4 @@ while True :
     for step in full_step_sequence:             # pour chacune des etapes
         for i in range(len(full_step_sequence)):# i de 0 à 3
             pins[i].value(step[i])	        # maj PIN n° i
-            sleep_ms(1)			        # il faut attendre un peu pour laisser le temps au moteur de deplacer
+        sleep_ms(2)			        # il faut attendre un peu pour laisser le temps au moteur de deplacer

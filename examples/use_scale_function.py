79fbd2ea246cbc3cd6a6095f96758ffae7f7858b
dfasani <david.fasani@gmail.com>
2023-01-09 16:30:03 +0100

    Update and rename use_map_function.py to use_scale_function.py

--- examples/use_scale_function.py
+++ examples/use_scale_function.py
@@ -4,6 +4,6 @@ from fasapico import *
 x = 30000
 
 #map the value x from the interval [0-65535] to the interval [0,100]
-mappedValue = map(x , 0 , 65535 , 0 , 100)
+mappedValue = scale(x , 0 , 65535 , 0 , 100)
 
 print( x , "-->" , mappedValue)

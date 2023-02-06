2cbd98c0ca792ee215f16939d4f7a647555b634f
dfasani <david.fasani@gmail.com>
2023-02-06 14:19:02 +0100

    print("ADC: " + str(reading))

--- examples/read_potentiometer.py
+++ examples/read_potentiometer.py
@@ -4,5 +4,5 @@ analog_value = machine.ADC(27)
  
 while True:
     reading = analog_value.read_u16()   #value between [0-65535]  
-    print("ADC: ",reading)
+    print("ADC: " + str(reading))
     utime.sleep(0.2)                    #do not read to frequentrly

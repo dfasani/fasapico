3045594a8ecc85c39924458b2c50b275cca2c13a
dfasani <david.fasani@gmail.com>
2023-02-07 10:31:43 +0100

    analog_value --> adc

--- examples/read_potentiometer.py
+++ examples/read_potentiometer.py
@@ -1,8 +1,8 @@
 import machine, utime
  
-analog_value = machine.ADC(27)
+adc = machine.ADC(27) #use the CAN on Pin 27
  
 while True:
-    reading = analog_value.read_u16()   #value between [0-65535]  
+    reading = adc.read_u16()   #value between [0-65535]  
     print("ADC: " + str(reading))
     utime.sleep(0.2)                    #do not read to frequentrly

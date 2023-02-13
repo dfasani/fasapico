538af42c2c809a0585c03637c584f32c647d863d
dfasani <david.fasani@gmail.com>
2023-02-13 15:57:52 +0100

    MIN --> BABORD

--- examples/servo.py
+++ examples/servo.py
@@ -4,20 +4,21 @@ from time import sleep
 servo = PWM(Pin(7)) # servo motor attached PIN 7
 servo.freq(50)      # 50 Hz is a good frequency
 
-MIN = 1000
-MID = 5000
-MAX = 9000
+BABORD = 3000
+BARRE_AU_CENTRE = 5000
+TRIBORD = 9000
 
-servo.duty_u16(MID)
+servo.duty_u16(BARRE_AU_CENTRE)
 sleep(1)
 
-servo.duty_u16(MIN)
+servo.duty_u16(BABORD)
 sleep(1)
 
-servo.duty_u16(MID)
+servo.duty_u16(BARRE_AU_CENTRE)
 sleep(1)
 
-servo.duty_u16(MAX)
+servo.duty_u16(TRIBORD)
 sleep(1)
 
-servo.duty_u16(MID)
+servo.duty_u16(BARRE_AU_CENTRE)
+print("Je reste cap au centre !")

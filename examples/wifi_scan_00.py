b35f1f07bb7806326d3550020948d5f2efa5bb8d
dfasani <david.fasani@gmail.com>
2023-03-13 13:22:45 +0100

    Update wifi_scan_00.py
    coquille

--- examples/wifi_scan_00.py
+++ examples/wifi_scan_00.py
@@ -2,6 +2,6 @@ import network
 
 wlan = network.WLAN(network.STA_IF) #  carte WiFi en mode station
 wlan.active(True)
-networkList = wlan.scan() # list de tuples de 6 champs : ssid, bssid, channel, RSSI, security, hidden
+networkList = wlan.scan() # des tuples de 6 champs : ssid, bssid, channel, RSSI, security, hidden
 
 print(networkList) #affiche la liste

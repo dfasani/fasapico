8c4dddbf360b97c3fe3e5b40615fd768522c1664
dfasani <david.fasani@gmail.com>
2023-03-13 13:28:38 +0100

    Add files via upload

---
+++ examples/wifi_connect_AP.py
@@ -0,0 +1,16 @@
+import network
+import time
+
+#change si necessaire
+WIFI_SSID = "icam_iot"
+WIFI_PASSWORD = "Summ3#C@mp2022"
+
+wlan = network.WLAN(network.STA_IF) #carte reseau en mode STATION
+wlan.connect(WIFI_SSID, WIFI_PASSWORD)
+
+while not wlan.isconnected() and wlan.status() >= 0:
+    print("J'essaie de me connecter...")
+    time.sleep(1)
+
+print(wlan.ifconfig())
+print("C'est bon ! Mon adresse IP est" , wlan.ifconfig()[0])
---
+++ examples/wifi_connect_AP_robust.py
@@ -0,0 +1,26 @@
+import network
+import time
+
+#change si necessaire
+WIFI_SSID = "icam_iot"
+WIFI_PASSWORD = "Summ3#C@mp2022"
+
+wlan = network.WLAN(network.STA_IF) #carte reseau en mode STATION
+wlan.connect(WIFI_SSID, WIFI_PASSWORD)
+
+# Wait for connect or fail
+max_wait = 10
+while max_wait > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    max_wait -= 1
+    print('waiting for connection...')
+    time.sleep(1)
+
+# Handle connection error
+if wlan.status() != 3:
+    raise RuntimeError('network connection failed')
+else:
+    print('connected')
+    status = wlan.ifconfig()
+    print( 'ip = ' + status[0] )

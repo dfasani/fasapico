f6c08b34efe6a46c59e30c7ff15d03e00bb659a9
dfasani <david.fasani@gmail.com>
2023-03-13 15:31:07 +0100

    Add files via upload

---
+++ examples/mqtt_hiveMQ_publish_public.py
@@ -0,0 +1,13 @@
+from mqtt import MQTTClientSimple
+from phew import connect_to_wifi
+from time import sleep
+
+WIFI_SSID = "icam_iot"
+WIFI_PASSWORD = "Summ3#C@mp2022"
+
+ip = connect_to_wifi(WIFI_SSID, WIFI_PASSWORD)
+
+clientMQTT = MQTTClientSimple(client_id="tonPrenomTonNomIci", server="broker.hivemq.com")
+clientMQTT.connect()
+clientMQTT.publish(topic="bretagne/a3" , msg="bientot le WE !")
+clientMQTT.disconnect()

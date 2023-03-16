79ae5b61bfe8baa4b1fdba0fa17e035e6cda3adf
dfasani <david.fasani@gmail.com>
2023-03-16 11:32:19 +0100

    Add files via upload

---
+++ examples/5e remote Pin Control.py
@@ -0,0 +1,31 @@
+from mqtt import TBDeviceMqttClient
+from phew import connect_to_wifi
+from machine import Pin
+
+WIFI_SSID = "Bbox-D9D06A65"
+WIFI_PASSWORD = "R6W4KEEC41yuPC7t9P"
+
+ip = connect_to_wifi(WIFI_SSID, WIFI_PASSWORD)
+
+
+led = Pin("LED")
+
+# Callback for server RPC requests
+# cette fonction est appelée lorsque l'utilisateur agit dans le dashboard
+def rpc_request_callback(client, request_id, request_body):
+    print("received rpc: id="+ str(request_id) + " request_body=" + str(request_body))  
+    
+# Connect to ThingsBoard
+client = TBDeviceMqttClient('demo.thingsboard.io', access_token='cleTopSecreteNePasDiffuser')
+
+#set callback for rpc resquests
+client.set_server_side_rpc_request_handler(rpc_request_callback)
+
+# Connecting to ThingsBoard
+client.connect()
+
+while True :
+    client.check_msg()
+
+# Disconnecting from ThingsBoard
+client.disconnect()

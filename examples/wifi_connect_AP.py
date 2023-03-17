cc02c6d520305ebcfca26b5607a9fac2926d8f6e
dfasani <david.fasani@gmail.com>
2023-03-17 08:37:40 +0100

    Update wifi_connect_AP.py

--- examples/wifi_connect_AP.py
+++ examples/wifi_connect_AP.py
@@ -6,6 +6,7 @@ WIFI_SSID = "icam_iot"
 WIFI_PASSWORD = "Summ3#C@mp2022"
 
 wlan = network.WLAN(network.STA_IF) #carte reseau en mode STATION
+wlan.active(True) # allume la carte réseau
 wlan.connect(WIFI_SSID, WIFI_PASSWORD)
 
 while not wlan.isconnected() and wlan.status() >= 0:

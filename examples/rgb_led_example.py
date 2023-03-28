09d503fcac795026689bf474073450041b10e07b
dfasani <david.fasani@gmail.com>
2023-03-28 14:24:29 +0200

    Update rgb_led_example.py

--- examples/rgb_led_example.py
+++ examples/rgb_led_example.py
@@ -1,35 +1,38 @@
 from maker_rgb_led import *
-import time
+from time import sleep
 
-while True:
-    print("off")
-    rgbLed(0,0,0)
-    time.sleep_ms(2000)
-    
-    print("red")
-    rgbLed(255,0,0)
-    time.sleep_ms(2000)
-    
-    print("green")
-    rgbLed(0,255,0)
-    time.sleep_ms(2000)
+print("off")
+rgbLed(0,0,0)
+sleep(2)
 
-    print("blue")
-    rgbLed(0,0,255)
-    time.sleep_ms(2000)
-    
-    print("yellow")
-    rgbLed(255,255,0)
-    time.sleep_ms(2000)
+print("red")
+rgbLed(255,0,0)
+sleep(2)
 
-    print("purple")
-    rgbLed(255,0,255)
-    time.sleep_ms(2000)
-    
-    print("cyan")
-    rgbLed(0,255,255)
-    time.sleep_ms(2000) 
+print("green")
+rgbLed(0,255,0)
+sleep(2)
 
-    print("white")
-    rgbLed(255,255,255)
-    time.sleep_ms(2000)
+print("blue")
+rgbLed(0,0,255)
+sleep(2)
+
+print("yellow")
+rgbLed(255,255,0)
+sleep(2)
+
+print("purple")
+rgbLed(255,0,255)
+sleep(2)
+
+print("cyan")
+rgbLed(0,255,255)
+sleep(2) 
+
+print("white")
+rgbLed(255,255,255)
+sleep(2)
+
+print("off")
+rgbLed(0,0,0)
+sleep(2)

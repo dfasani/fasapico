d2fc04ab9d4e7c14432ac0b6a63adbaab713010c
dfasani <david.fasani@gmail.com>
2023-04-13 12:02:28 +0200

    Create http_get - JSON - quotable - student.py

---
+++ examples/http_get - JSON - quotable - student.py
@@ -0,0 +1,24 @@
+from fasapico import *
+
+#
+# 1. Je me connecte au WiFi
+#
+
+#change si necessaire
+WIFI_SSID = "icam_iot"
+WIFI_PASSWORD = "Summ3#C@mp2022"
+
+ip = connect_to_wifi(WIFI_SSID, WIFI_PASSWORD)
+#print("Connected to WiFi (" +ip+")")
+
+#
+# 2. J'envoie une requete
+#
+
+jsonData = get_json_from_url(url = "https://api.quotable.io/random")
+
+#
+# 3. J'exploite le résultat
+#
+print(jsonData) #tout afficher
+print(jsonData['authorSlug'] ) #afficher l'auteur uniquement

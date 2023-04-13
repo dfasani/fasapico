8fe45787ae4002fe5e0486ac7b90b9c3a67ffbdc
dfasani <david.fasani@gmail.com>
2023-04-13 11:37:21 +0200

    add wifi methods

--- libs/fasapico.py
+++ libs/fasapico.py
@@ -6,3 +6,55 @@ def scale(x, in_min, in_max, out_min, out_max):
 # scale a value x from one range [in_min, in_max] to a new range [out_min, out_max], return an integer
 def scale_to_int(x, in_min, in_max, out_min, out_max):
     return int(scale(x, in_min, in_max, out_min, out_max))
+
+
+def is_connected_to_wifi():
+  import network, time
+  wlan = network.WLAN(network.STA_IF)
+  return wlan.isconnected()
+
+# helper method to quickly get connected to wifi
+def connect_to_wifi(ssid, password, timeout_seconds=30):
+  import network, time
+
+  statuses = {
+    network.STAT_IDLE: "idle",
+    network.STAT_CONNECTING: "connecting",
+    network.STAT_WRONG_PASSWORD: "wrong password",
+    network.STAT_NO_AP_FOUND: "access point not found",
+    network.STAT_CONNECT_FAIL: "connection failed",
+    network.STAT_GOT_IP: "got ip address"
+  }
+
+  wlan = network.WLAN(network.STA_IF)
+  wlan.active(True)    
+  wlan.connect(ssid, password)
+  start = time.ticks_ms()
+  status = wlan.status()
+
+  
+  while not wlan.isconnected() and (time.ticks_ms() - start) < (timeout_seconds * 1000):
+    new_status = wlan.status()
+    if status != new_status:
+      status = new_status
+    time.sleep(0.25)
+
+  if wlan.status() != 3:
+    return None
+
+  return wlan.ifconfig()[0]
+
+# helper method to put the pico into access point mode
+def access_point(ssid, password = None):
+  import network
+
+  # start up network in access point mode  
+  wlan = network.WLAN(network.AP_IF)
+  wlan.config(essid=ssid)
+  if password:
+    wlan.config(password=password)
+  else:    
+    wlan.config(security=0) # disable password
+  wlan.active(True)
+
+  return wlan

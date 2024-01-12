fbbf6d09a814d2f627b400e277a60fcca6f28133
dfasani <david.fasani@gmail.com>
2024-01-12 10:19:23 +0100

    Create grove_lcd_example.py

---
+++ examples/grove_lcd_example.py
@@ -0,0 +1,12 @@
+from utime import sleep
+from grove_lcd_i2c import *
+
+#screen init
+lcd = Grove_LCD_I2C()
+
+#erase the screen
+lcd.clear()
+
+#goes to coordinates (0,0 = first line, first column)
+lcd.home()
+lcd.write("     David  \n     Fasani")

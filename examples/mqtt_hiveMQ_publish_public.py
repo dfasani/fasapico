8ed05a0dc76ea8e5b2204acbf7e9a1d50b1503fb
dfasani <david.fasani@gmail.com>
2024-01-16 11:46:00 +0100

    Update mqtt_hiveMQ_publish_public.py

--- examples/mqtt_hiveMQ_publish_public.py
+++ examples/mqtt_hiveMQ_publish_public.py
@@ -1,14 +1,20 @@
 from mqtt import MQTTClientSimple
-from phew import connect_to_wifi
-from time import sleep
+from fasapico import *
 
-WIFI_SSID = "icam_iot"
-WIFI_PASSWORD = "Summ3#C@mp2022"
+ip = connect_to_wifi(ssid="icam_iot", password = "Summ3#C@mp2022")
 
-ip = connect_to_wifi(WIFI_SSID, WIFI_PASSWORD)
+
+#												#
+#	A TOI DE REMPLACER "tonPrenomTonNomIci"		#
+#	SINON ON SE FAIT JETER PAR LE SERVEUR !		#
+#												#
 
 #merci de remplacer "tonPrenomTonNomIci" , sinon on va se faire jeter par le serveur
 clientMQTT = MQTTClientSimple(client_id="tonPrenomTonNomIci", server="broker.hivemq.com")
 clientMQTT.connect()
 clientMQTT.publish(topic="bretagne/a3" , msg="bientot le WE !") #tu peux changer de topic et de msg !
 clientMQTT.disconnect()
+
+print("deja fini !!!")
+
+# TU PEUX ALLER SUR https://www.hivemq.com/demos/websocket-client/  pour voir ton message passer !

9012062868d5a024f7c8a1270912ab35c20c60a8
dfasani <david.fasani@gmail.com>
2024-01-16 11:20:43 +0100

    utilisation de connect_to_wifi() de fasapico

--- examples/wifi_connect_AP.py
+++ examples/wifi_connect_AP.py
@@ -1,17 +1,6 @@
-import network
-import time
+from fasapico import *
 
-#change si necessaire
-WIFI_SSID = "icam_iot"
-WIFI_PASSWORD = "Summ3#C@mp2022"
+#change les parametres si besoin est
+adresse_ip = connect_to_wifi(ssid = "icam_iot" , password = "Summ3#C@mp2022" ) 
 
-wlan = network.WLAN(network.STA_IF) #carte reseau en mode STATION
-wlan.active(True) # allume la carte réseau
-wlan.connect(WIFI_SSID, WIFI_PASSWORD)
-
-while not wlan.isconnected() and wlan.status() >= 0:
-    print("J'essaie de me connecter...")
-    time.sleep(1)
-
-print(wlan.ifconfig())
-print("C'est bon ! Mon adresse IP est" , wlan.ifconfig()[0])
+print("C'est bon ! Mon adresse IP est " + adresse_ip)

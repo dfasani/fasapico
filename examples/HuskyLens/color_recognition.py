9c30c98731bdf021b8983c578b49ec6efb698369
dfasani <david.fasani@gmail.com>
2024-01-26 10:38:23 +0100

    Update color_recognition.py

--- examples/HuskyLens/color_recognition.py
+++ examples/HuskyLens/color_recognition.py
@@ -11,8 +11,8 @@ while True:
     reponseHuskyLens = husky.command_request()
     
     if len(reponseHuskyLens) > 0 :
-        x 		= reponseHuskyLens[0][0]     #asbcisse du centre du carré
-        y 		= reponseHuskyLens[0][1]     #ordonnée du centre du carré
+        x 		= reponseHuskyLens[0][0]     #asbcisse du centre du carré : entre 0 (gauche) et 320 (droite)
+        y 		= reponseHuskyLens[0][1]     #ordonnée du centre du carré : entre 0 (haut) et 240 (bas)
         largeur	= reponseHuskyLens[0][2] #largeur du carré
         hauteur	= reponseHuskyLens[0][3] #hauteur du carré
         

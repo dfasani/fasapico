b5fea3b8df583847603fefc040209acf3b7617ef
dfasani <david.fasani@gmail.com>
2024-02-16 17:15:36 +0100

    Update use_scale_function.py

--- examples/use_scale_function.py
+++ examples/use_scale_function.py
@@ -1,9 +1,9 @@
 from fasapico import *
 
-#change this value from [0 , 65535]
-x = 30000
+# l'utilisateur donne une valeur dans l'intervale [0 , 65535]
+valeurEntreOet65535 = int(input("Donne une valeur entre [0 , 65535]"))
 
-#map the value x from the interval [0-65535] to the interval [0,100]
-mappedValue = scale(x , 0 , 65535 , 0 , 100)
+#mise a l'échelle dans l'intervalle [0,100]
+valeurEntreOet100 = scale(valeurEntreOet65535 , 0 , 65535 , 0 , 100)
 
-print( x , "-->" , mappedValue)
+print( valeurEntreOet65535 , " donne " , valeurEntreOet100 , " %" )

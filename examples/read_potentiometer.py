1eef780af241c22f3f97337ecc1e66d418ebcb05
dfasani <david.fasani@gmail.com>
2024-02-16 16:49:28 +0100

    Update read_potentiometer.py

--- examples/read_potentiometer.py
+++ examples/read_potentiometer.py
@@ -1,8 +1,9 @@
-import machine, time
+from time import *
+from machine import *
  
-adc = machine.ADC(27) #use the CAN on Pin 27
+adc = ADC(27) #use the CAN on Pin 27
  
 while True:
     reading = adc.read_u16()   #value between [0-65535]  
     print("ADC: " + str(reading))
-    time.sleep(0.2)                    #do not read to frequentrly
+    sleep(0.2)     #do not read to frequentrly

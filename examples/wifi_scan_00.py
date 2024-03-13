63e2c0a50b6b8685d353929b58f6a7d1c898dec7
dfasani <david.fasani@gmail.com>
2024-03-13 11:31:18 +0100

    Update wifi_scan_00.py

--- examples/wifi_scan_00.py
+++ examples/wifi_scan_00.py
@@ -4,4 +4,4 @@ wlan = network.WLAN(network.STA_IF) #  carte WiFi en mode station
 wlan.active(True)
 networkList = wlan.scan() # des tuples de 6 champs : ssid, bssid, channel, RSSI, security, hidden
 
-print(networkList) #affiche la liste A TOI DE CHANGER CETTE PARTIE
+print(networkList) #affiche la liste TU PEUX SUPPRIMER CETTE LIGNE AU 1A

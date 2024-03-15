f900887773a345dffea81797b7f2829a7ce7289f
dfasani <david.fasani@gmail.com>
2024-03-15 09:22:09 +0100

    Update http_get - JSON - genderize - student.py

--- examples/http_get - JSON - genderize - student.py
+++ examples/http_get - JSON - genderize - student.py
@@ -1,39 +1,19 @@
-from fasapico import connect_to_wifi
-
-
-def get_json_from_url(url):
-    import urequests, json
-    
-    #retirer le commentaire pour debuger
-    #print("Sending request to :",url)
-    
-    reponse = urequests.get(url)
-    contenuDeLaReponse = reponse.content #recupere le corps de la reponse
-    reponse.close() # <-- ULTRA IMPORTANT : FERMER LA REPONSE SINON ON SE FAIT JETER PAR SERVEUR A LA SUIVANTE !!!
-    
-    #retirer le commentaire pour debuger
-    #print(corps)
-    
-    jsonData = json.loads(contenuDeLaReponse)
-    return jsonData
-
-#
-# 1. Je me connecte au WiFi
-#
-
+from fasapico import *
 import network
-#change si necessaire
-WIFI_SSID = "icam_iot"
-WIFI_PASSWORD = "Summ3#C@mp2022"
 
-ip = connect_to_wifi(WIFI_SSID, WIFI_PASSWORD)
+#
+# 1. Je me connecte au WiFi     change si necessaire
+#
+
+ip = connect_to_wifi("icam_iot", "Summ3#C@mp2022")
 print(ip)
 
 #
 # 2. J'envoie une requete
 #
-
-jsonData = get_json_from_url(url = "https://api.genderize.io/?name=David")
+urlGenderize = "https://api.genderize.io/?name=David"
+print(urlGenderize)
+jsonData = get_json_from_url(urlGenderize)
 
 #
 # 3. J'explore le résultat

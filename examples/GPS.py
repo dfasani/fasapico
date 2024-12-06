811619d51c1cfcc558cbd011bee2fa0a0131a0c0
dfasani <david.fasani@gmail.com>
2024-12-06 11:25:27 +0100

    Update GPS.py

--- examples/GPS.py
+++ examples/GPS.py
@@ -1,15 +1,17 @@
-from machine import Pin, UART
-from time import sleep
-from micropyGPS import MicropyGPS
+from machine import *
+from time import *
+from micropyGPS import *
 
 gpsModule = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
-my_gps = MicropyGPS()
-    
+gpsDecoder = MicropyGPS()
+
+print("Il faut aller dehors et attendre 1-3 minutes pour avoir un fix GPS")
+
 while True:
     
     #maj donnees GPS
     for x in str(gpsModule.readline()):
-        my_gps.update(x)
+        gpsDecoder.update(x)
     
     print( "latitude" + str( my_gps.latitude) )
     print( "longitude"+ str( my_gps.longitude) )

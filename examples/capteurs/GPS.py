f2d29fdbae63c8b240794dc2f88a94660798380a
dfasani <david.fasani@gmail.com>
2025-06-13 15:27:51 +0200

    Update GPS.py
    ajout commentaire si braché sur les broches 4 et 5

--- examples/capteurs/GPS.py
+++ examples/capteurs/GPS.py
@@ -5,7 +5,12 @@ from micropyGPS import *
 #micropyGPS disponible ici : https://github.com/inmcm/micropyGPS/blob/master/micropyGPS.py
 #pour l'utiliser, il faut le copier dans le dossier lib de votre pico
 
+#si votre GPS est branché sur les pin 0 et 1
 gpsModule = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
+
+#si votre GPS est branché sur les pin 4 et 5
+#gpsModule = UART(0, baudrate=9600, tx=Pin(4), rx=Pin(5))
+
 gpsDecoder = MicropyGPS()
 
 print("Il faut aller dehors et attendre 1-3 minutes pour avoir un fix GPS")

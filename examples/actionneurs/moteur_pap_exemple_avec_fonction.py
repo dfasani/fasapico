6b2db1a5fa0f6c734cd7dad56c91783e31b0f04a
fasapico <david.fasani@icam.fr>
2025-12-10 14:06:57 +0100

    feat: add example demonstrating stepper motor control using fasapico.Stepper

--- examples/actionneurs/moteur_pap_exemple_avec_fonction.py
+++ examples/actionneurs/moteur_pap_exemple_avec_fonction.py
@@ -1,5 +1,5 @@
-from machine import *
-from time import *
+from fasapico import Stepper
+from time import sleep_ms
 
 #################################
 #                               #
@@ -11,44 +11,8 @@ from time import *
 # PARTIE STEPPER #
 # -------------- #
 
-pins = [
-        Pin(10,Pin.OUT),
-        Pin(11,Pin.OUT),
-        Pin(12,Pin.OUT),
-        Pin(13,Pin.OUT)
-    ]
-
-#############################
-#                           #
-# DEFINITION DES FONCTIONS  #
-#                           #
-#############################   
-
-def runStepper(nbPas):
-    
-    if nbPas > 0 :
-        steps_sequence = [     
-            [1,0,0,0],
-            [0,1,0,0],
-            [0,0,1,0],
-            [0,0,0,1]
-        ]
-        
-    else:
-        steps_sequence = [     
-            [0,0,0,1],
-            [0,0,1,0],
-            [0,1,0,0],
-            [1,0,0,0]
-        ]
-        
-    
-    for _ in range(abs(nbPas)):
-        for step in steps_sequence:     # pour chacune des etapes
-                for i in range(4):              # i de 0 à 3
-                    pins[i].value(step[i])	# maj PIN n° i
-                sleep_ms(5	)			# il faut attendre un peu pour laisser le temps au moteur de deplacer
-
+# Utilisation de la classe Stepper de la librairie fasapico
+stepper = Stepper() # Pins par defaut: 10, 11, 12, 13
 
 ######################
 #                    #
@@ -58,9 +22,9 @@ def runStepper(nbPas):
 
 while True :
   print("a droite")
-  runStepper(-100)
+  stepper.move(-100)
   sleep_ms(500)
   
   print("a gauche")
-  runStepper(100)
+  stepper.move(100)
   sleep_ms(500)

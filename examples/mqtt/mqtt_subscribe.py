34f0943c09144db24d58807d4997feef26d0e5bb
fasanicam <57295448+fasanicam@users.noreply.github.com>
2025-12-22 17:12:27 +0100

    rempalcement

--- examples/mqtt/mqtt_subscribe.py
+++ examples/mqtt/mqtt_subscribe.py
@@ -1,19 +1,27 @@
 from fasapico import *
-import secrets
-ip = connect_to_wifi()
 
-#on dit que c'est un callback : cette fonction est donc appelée automatiquement a chaque reception de msg
-def on_message_callback(topic,msg):
-    topic = topic.decode('utf-8')
-    msg = msg.decode('utf-8')
-    print("recu >>",topic,msg)
-    
-#merci de remplacer "tonPrenomTonNomIci" , sinon on va se faire jeter par le serveur
-clientMQTT = MQTTClientSimple(client_id="tonPrenomTonNomIci", server="mqtt.dev.icam.school", ssl=True)
-clientMQTT.connect() #start connection
-clientMQTT.set_callback(on_message_callback) #a chaque evenement, on appelle la fonction on_message_callback()
-clientMQTT.subscribe("bzh/iot/demo/maquette/a3") #on s'abonne a bzh/iot/demo/maquette/a3
+# Connexion au WiFi
+ip = connect_to_wifi(ssid = "icam_iot" , password = "Summ3#C@mp2022")
+print(f"Connecté au WiFi avec l'ip {ip}")
 
-while True :
-    clientMQTT.wait_msg() #on regarde la boite aux lettres si un msg est arrivé
+# Callback : fonction appelée automatiquement à chaque réception d'un message
+def on_message_callback(topic, msg):
+    print(f"Message reçu >> Topic : {topic} | Contenu : {msg}")
+
+# Initialisation du client MQTT (remplacer `tonPrenomTonNomIci` par un identifiant unique)
+clientMQTT = MQTTClientSimple(client_id="tonPrenomTonNomIci", server="mqtt.dev.icam.school")
+clientMQTT.connect()
+clientMQTT.set_callback(on_message_callback)
+clientMQTT.subscribe("bde/americam")
+
+print()
+print("Abonné au topic : bde/americam")
+
+# Message pour indiquer comment quitter le script proprement
+print("En attente des messages... Appuyez sur Ctrl+C pour quitter.")
+
+# Boucle principale pour attendre les messages
+while True:
+    clientMQTT.wait_msg()
     #n'ajoute pas de code ici stp --> c'est la fonction on_message_callback() qu'on va completer
+    #si tu n'as pas pris en compte ma consigne et que tu m'appelle je vais doubler tes frais de vie :)

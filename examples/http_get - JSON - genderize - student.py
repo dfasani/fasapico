179ea0fe81fe4f3cc3dd88bdab56db313460f27f
fasanicam <57295448+fasanicam@users.noreply.github.com>
2025-12-22 16:24:56 +0100

    Update URL for genderize API request

--- examples/http_get - JSON - genderize - student.py
+++ examples/http_get - JSON - genderize - student.py
@@ -1,17 +1,16 @@
 from fasapico import *
-import network
 
 #
 # 1. Je me connecte au WiFi     change si necessaire
 #
 
 ip = connect_to_wifi("icam_iot", "Summ3#C@mp2022")
-print(ip)
+print(f"Adresse IP : {ip}")
 
 #
 # 2. J'envoie une requete
 #
-urlGenderize = "https://api.genderize.io/?name=David"
+urlGenderize = "https://bzh.dev.icam.school/genderize?name=David"
 print(urlGenderize)
 jsonData = get_json_from_url(urlGenderize)
 

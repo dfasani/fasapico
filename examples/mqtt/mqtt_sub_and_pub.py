a5e11b00bb36cafabb028508d2f8a7ce39cc3a1f
fasanicam <57295448+fasanicam@users.noreply.github.com>
2025-12-22 17:17:59 +0100

    Refactor MQTT subscriber and publisher script

---
+++ examples/mqtt/mqtt_sub_and_pub.py
@@ -0,0 +1,38 @@
+    from machine import *
+    from fasapico import *
+
+    # Connexion au WiFi
+    ip = connect_to_wifi(ssid = "icam_iot" , password = "Summ3#C@mp2022")
+    print(f"Connecté au WiFi avec l'ip {ip}")
+
+    def on_message_callback(topic,msg):
+        print(f"Message reçu >> Topic : {topic} | Contenu : {msg}")
+
+
+    #merci de remplacer "DavidFasani" , sinon on va se faire jeter par le serveur
+    clientMQTT = MQTTClientSimple(client_id="DavidFasani", server="mqtt.dev.icam.school")
+    clientMQTT.set_callback(on_message_callback)  #que doit-on faire à reception d'un message ? Appeler la fonction on_message_callback()
+    clientMQTT.connect()
+
+    print()
+    print("Je m'abonne a quel topic ?")
+    topic = input()
+    clientMQTT.subscribe(topic)
+
+    def timer_callback(data):
+        clientMQTT.check_msg() #on releve la boite aux lettres
+        
+    #ce timer appelle timer_callback() toutes les 100ms en arriere plan, on a plus besoin de gérer les appels =)
+    Timer(mode=Timer.PERIODIC, period=100, callback=timer_callback)
+
+    #Je demande en boucle
+    while True :
+        print()
+        print("Je publie sur quel topic ?")
+        topic = input()
+        print("Quel message ?")
+        msg = input()
+        clientMQTT.publish( topic , msg)
+
+
+
